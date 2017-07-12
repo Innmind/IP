0a21fe1eb51da8c0930a5edb6171985637957856
Baptiste Langlade <baptouuuu@gmail.com>
2017-07-12 08:16:00 +0200

    add ipv4 model

---
+++ src/IPv4.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\IP;
+
+use Innmind\IP\Exception\AddressNotMatchingIPv4Format;
+
+final class IPv4
+{
+    private $address;
+
+    public function __construct(string $address)
+    {
+        if (!filter_var($address, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
+            throw new AddressNotMatchingIPv4Format($address);
+        }
+
+        $this->address = $address;
+    }
+
+    public static function localhost(): self
+    {
+        return new self('127.0.0.1');
+    }
+
+    public function equals(self $self): bool
+    {
+        return $this->address === $self->address;
+    }
+
+    public function __toString(): string
+    {
+        return $this->address;
+    }
+}
